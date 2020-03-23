264f80d0c76c2a4c401f6e4157e105bd44c0b0b1
Sridhar Pedamallu <sridharpedamallu@gmail.com>
2020-03-23 12:18:11 +0530

    a

--- classes/db.class.php
+++ classes/db.class.php
@@ -6,17 +6,18 @@ class db {
 
     function __construct(){
 
-        $server = "localhost";
-        $dbName = "pas_uat";
+        $server = "localhost\SQLEXPRESS";
+        // $server = "localhost";
+        // $dbName = "pas_uat";
+        $dbName = "pas_dev";
         $userName = "pasuser";
         $password = "";
 
-        $this->connection = new PDO("sqlsrv:Server=localhost;Database=pas_uat", $userName, $password);
+        // $this->connection = new PDO("sqlsrv:Server=localhost;Database=pas_uat", $userName, $password);
+        $this->connection = new PDO("sqlsrv:Server=DESKTOP-2K1UP0H\SQLEXPRESS;Database=pas_dev", $userName, $password);
     }
 
     public function getConnection() {
         return $this->connection;
     }
-
-
 }
